200a9a0830c77f65a409b8846c0410a11265c0ff
aymane <berrada98aymane@gmail.com>
2023-03-16 14:57:11 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a c program that print with a put funtion
+ *
+ * Return: always 0 (Success)
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
